a469fdb4253c6cd968232e055e4997d132f75b7f
muhammadasif2017 <muhammadasifhaqnawaz2019@gmail.com>
2022-08-15 15:01:07 +0500

    Array Problems: move zeroes

---
+++ arrays/move-zeroes.js
@@ -0,0 +1,15 @@
+function moveZeroes(nums) {
+  let index = 0;
+  for (let i = 0; i < nums.length; i++) {
+    if (nums[i] != 0) {
+      const temp = nums[i];
+      nums[i] = nums[index];
+      nums[index] = temp;
+      index++;
+    }
+  }
+  console.log('Nums:', nums);
+  return nums;
+}
+
+moveZeroes([1, 0, 2, 0, 3]);

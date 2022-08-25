b07f510a24bec354bdfc9b805f282f7141fe3c2e
muhammadasif2017 <muhammadasifhaqnawaz2019@gmail.com>
2022-08-26 01:49:38 +0500

    Dynamic Programming Problem: fibonacci problem

---
+++ dynamic-programming/fibonacci-numbers.js
@@ -0,0 +1,18 @@
+function fibonacciMaster() {
+  let cache = {};
+  return function fib(n) {
+    if (n in cache) {
+      return cache[n];
+    } else {
+      if (n < 2) {
+        return n;
+      } else {
+        cache[n] = fib(n-1) + fib(n-2);
+        return cache[n];
+      }
+    }
+  }
+}
+
+const fastFib = fibonacciMaster();
+console.log('DP', fastFib(10));

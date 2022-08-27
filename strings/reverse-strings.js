1e707f2adee5029cefdceb11aa6f93021d3ccc47
Muhammad Asif <muhammadasifhaqnawaz2019@gmail.com>
2022-08-27 18:57:10 +0500

    String Problems: in-place string reversal

--- strings/reverse-strings.js
+++ strings/reverse-strings.js
@@ -10,6 +10,8 @@ function reverse(str) {
     return "hmm that is not good";
   }
 
+  // O(n) memory space
+  // O(n) time complexity
   const backwards = [];
   const totalItems = str.length - 1;
   for (let i = totalItems; i >= 0; i -= 1) {
@@ -22,5 +24,19 @@ function reverse(str) {
 // function reverse2(str) {
 //   return str.split('').reverse().join('');
 // }
+
+/*
+  In-place traversal -> O(1) memory space
+  O(n) time complexity
+  const reverseString = function(s) {
+    const strLength = s.length - 1;
+    const counter = Math.ceil(strLength / 2);
+    for(let i = 0; i < counter ; i += 1) {
+        let temp =  s[strLength - i];
+        s[strLength - i] = s[i];
+        s[i] = temp;
+    }
+};
+*/
 
 console.log(reverse("Hi My name is Muhammad Asif"));

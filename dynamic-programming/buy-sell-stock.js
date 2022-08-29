d06340270494b0d4b2dbf6b3861675f4ced8106b
muhammadasif2017 <muhammadasifhaqnawaz2019@gmail.com>
2022-08-29 21:54:24 +0500

    Dynamic Programming Problem: buy and sell stock

--- dynamic-programming/buy-sell-stock.js
+++ dynamic-programming/buy-sell-stock.js
@@ -10,3 +10,18 @@ function maxProfit(prices){
   }
   return profit
 };
+
+/*
+const maxProfit = function(prices) {
+  if (!prices|| prices.length == 0) return 0;
+  let min = Number.MAX_SAFE_INTEGER,
+  let profit = 0;
+  for(let p of prices){
+      min = Math.min(p , min);
+      profit = Math.max(p - min, profit);
+  }
+  return profit;
+};
+
+
+*/
